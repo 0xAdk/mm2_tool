b9a985c501af3e2aff59920fc2a2e986957dab97
0xadk <0xadk@users.noreply.github.com>
2024-06-25 16:03:16 -0700

    fix: remove padding from decrypted data

--- src/main.rs
+++ src/main.rs
@@ -16,21 +16,39 @@ fn main() {
             println!("{obj:#?}");
         }
 
-        Some("csv") => {
-            let mut data = std::fs::read(cli.file).unwrap();
-            {
-                let data = match bytemuck::try_cast_slice_mut(&mut data) {
-                    Ok(data) => data,
-                    Err(err) => {
-                        todo!("{}", err)
-                    }
-                };
-
-                xxtea::decrypt(data, MM2_ASSET_KEY).unwrap();
-            }
-            let data = std::str::from_utf8(&data).unwrap();
-            println!("{}", data);
-        }
+        Some("csv") => match decrypt_mm2_asset(cli.file) {
+            Ok(s) => println!("{s}"),
+            Err(err) => println!("{err:?}"),
+        },
         _ => panic!("Unsupported file type"),
     }
+}
+
+#[derive(thiserror::Error, Debug)]
+enum AssetDecryptError {
+    #[error(transparent)]
+    FailedCast(#[from] bytemuck::PodCastError),
+
+    #[error(transparent)]
+    InvalidUtf8Data(#[from] std::str::Utf8Error),
+}
+
+fn decrypt_mm2_asset(file: std::path::PathBuf) -> Result<String, AssetDecryptError> {
+    let mut data = std::fs::read(file).unwrap();
+    {
+        let data = bytemuck::try_cast_slice_mut(&mut data)?;
+        xxtea::decrypt(data, MM2_ASSET_KEY).unwrap();
+    }
+
+    // pop at most 4 nul padding bytes from the end
+    for _ in 0..4 {
+        match data.last() {
+            Some(&0) => {
+                data.pop();
+            }
+            _ => break,
+        }
+    }
+
+    Ok(std::str::from_utf8(&data)?.to_string())
 }
