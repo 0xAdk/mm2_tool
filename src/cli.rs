47abdbacfdfddfa7b506104c6bc3971d5861ddd6
0xadk <0xadk@users.noreply.github.com>
2024-06-26 03:01:33 -0700

    feat: improve cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,44 @@
 use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 pub struct Cli {
-	#[arg(short, long)]
-	pub encrypt: bool,
+    #[command(subcommand)]
+    pub command: Command,
+}
 
-	pub file: PathBuf,
+#[derive(Subcommand)]
+pub enum Command {
+    Crypt {
+        // TODO: make this optional + global
+        // #[arg(short, long, global = true)]
+        #[arg(short, long)]
+        output: PathBuf,
+
+        #[command(subcommand)]
+        command: CryptCommand,
+    },
+
+    Haxe {
+        // TODO: make this optional + global
+        // #[arg(short, long, global = true)]
+        #[arg(short, long)]
+        output: PathBuf,
+
+        #[command(subcommand)]
+        command: HaxeCommand,
+    },
+}
+
+#[derive(Subcommand)]
+pub enum CryptCommand {
+    Encrypt { file: PathBuf },
+    Decrypt { file: PathBuf },
+}
+
+#[derive(Subcommand)]
+pub enum HaxeCommand {
+    Serialize { file: PathBuf },
+    Deserialize { file: PathBuf },
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![deny(clippy::pedantic)]
 
 mod cli;
+mod crypt;
 mod haxe_obj;
 mod xxtea;
 
@@ -9,38 +10,34 @@ const MM2_ASSET_KEY: &[u8; 16] = b"aj3fk29dl309f845";
 fn main() {
     let cli = <cli::Cli as clap::Parser>::parse();
 
-    match cli.file.extension().unwrap().to_str() {
-        Some("dat") => {
-            let data = std::fs::read_to_string(cli.file).unwrap();
-            let data = &mut data.as_str();
-
-            let obj = haxe_obj::parse(data).unwrap();
-            println!("{obj:#?}");
-        }
-
-        Some("csv" | "txt") => {
-            if cli.encrypt {
-                let data = std::fs::read_to_string(cli.file).unwrap();
-
-                match encrypt_mm2_asset(&data) {
-                    Err(err) => panic!("{err:?}"),
-                    Ok(data) => {
-                        std::fs::write("output.encrypt.csv", data).unwrap();
-                    }
-                }
-            } else {
-                let data = std::fs::read(cli.file).unwrap();
-
-                match decrypt_mm2_asset(data) {
-                    Err(err) => panic!("{err:?}"),
-                    Ok(s) => {
-                        print!("{s}");
-                    }
-                }
+    match cli.command {
+        cli::Command::Crypt {
+            command, output, ..
+        } => match command {
+            cli::CryptCommand::Encrypt { file, .. } => {
+                let data = std::fs::read(file).unwrap();
+                let data = encrypt_mm2_asset(data).unwrap_or_else(|err| panic!("{err:?}"));
+                std::fs::write(output, data).unwrap();
             }
-        }
 
-        _ => panic!("Unsupported file type"),
+            cli::CryptCommand::Decrypt { file, .. } => {
+                let data = std::fs::read(file).unwrap();
+                let data = decrypt_mm2_asset(data).unwrap_or_else(|err| panic!("{err:?}"));
+                std::fs::write(output, data).unwrap();
+            }
+        },
+        cli::Command::Haxe { command, output, .. } => match command {
+            cli::HaxeCommand::Serialize { file: _, .. } => {
+                todo!("serializing files isn't implemented yet")
+            }
+            cli::HaxeCommand::Deserialize { file, .. } => {
+                let data = std::fs::read_to_string(file).unwrap();
+                let data = &mut data.as_str();
+
+                let obj = haxe_obj::parse(data).unwrap();
+                std::fs::write(output, std::format!("{obj:?}")).unwrap();
+            }
+        },
     }
 }
 
@@ -72,9 +69,7 @@ fn decrypt_mm2_asset(mut data: Vec<u8>) -> Result<String, AssetDecryptError> {
     Ok(std::str::from_utf8(&data)?.to_string())
 }
 
-fn encrypt_mm2_asset(data: &str) -> Result<Vec<u8>, AssetDecryptError> {
-    let mut data: Vec<u8> = data.bytes().collect();
-
+fn encrypt_mm2_asset(mut data: Vec<u8>) -> Result<Vec<u8>, AssetDecryptError> {
     {
         data.resize(data.len().next_multiple_of(4), 0);
         let data = bytemuck::try_cast_slice_mut(&mut data)?;
