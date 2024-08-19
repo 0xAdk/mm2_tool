14fda8c1e471d2238e83c8afcd215a2985c035a5
0xadk <0xadk@users.noreply.github.com>
2024-08-19 16:05:39 -0700

    feat(savetool): store hard-coded version in output format

--- src/savetool.rs
+++ src/savetool.rs
@@ -1,8 +1,11 @@
 use clap::Subcommand;
+use serde::Deserialize;
+use serde::Serialize;
 
 use crate::haxe;
 use crate::xxtea;
 
+use std::io::Write;
 use std::path::PathBuf;
 
 pub const MM2_SAVE_KEY: &str = "HXl;kjsaf4982097";
@@ -58,29 +61,25 @@ pub fn run(Cli::Savetool { command }: Cli) {
 
             let data = std::fs::read(file).unwrap();
 
-            let value: Vec<haxe::Value> = match format {
+            let save_file: SaveFile = match format {
                 haxe::FileFormat::Debug => unreachable!(),
 
                 #[cfg(feature = "export-json")]
                 haxe::FileFormat::Json => serde_json::from_slice(&data).unwrap(),
             };
 
-            let data = haxe::to_string(&value);
-
-            // TODO: we should save this tag on load instead of just hardcoding it here
-            let tag = "[4.0.104]";
-            let data = format!("{tag}{data}").into_bytes();
+            let data = SaveFile::save(&save_file);
 
             let key = MM2_SAVE_KEY.as_bytes().try_into().unwrap();
-            let data =
-                xxtea::encrypt_with_padding(data, key).unwrap_or_else(|err| panic!("{err:?}"));
+            let data = xxtea::encrypt_with_padding(data.into_bytes(), key)
+                .unwrap_or_else(|err| panic!("{err:?}"));
 
             std::fs::write(output, data).unwrap();
         }
 
         Command::Load {
             file,
-            output,
+            output: output_path,
             format,
         } => {
             let data = std::fs::read(file).unwrap();
@@ -89,29 +88,47 @@ pub fn run(Cli::Savetool { command }: Cli) {
             let mut data =
                 xxtea::decrypt_with_padding(data, key).unwrap_or_else(|err| panic!("{err:?}"));
 
-            let version_tag_end = data.iter().position(|c| *c == b']').unwrap();
-            let _ = data.drain(..=version_tag_end);
+            let save_file = SaveFile::load(&mut data);
 
-            let obj = haxe::from_str(std::str::from_utf8(&data).unwrap()).unwrap();
-
-            #[cfg_attr(not(feature = "export-json"), allow(unused_variables))]
-            let byte_vec_spot: Vec<u8>;
-            let string_spot: String;
-
-            let bytes = match haxe::FileFormat::guess(format, &output) {
+            let mut output = std::fs::File::create(&output_path).unwrap();
+            match haxe::FileFormat::guess(format, &output_path) {
                 haxe::FileFormat::Debug => {
-                    string_spot = format!("{obj:#?}");
-                    string_spot.as_bytes()
+                    write!(output, "{save_file:#?}").unwrap();
                 }
 
                 #[cfg(feature = "export-json")]
                 haxe::FileFormat::Json => {
-                    byte_vec_spot = serde_json::to_vec_pretty(&obj).unwrap();
-                    byte_vec_spot.as_slice()
+                    output
+                        .write_all(&serde_json::to_vec_pretty(&save_file).unwrap())
+                        .unwrap();
                 }
             };
+        }
+    }
+}
 
-            std::fs::write(output, bytes).unwrap();
+#[derive(Debug, Serialize, Deserialize)]
+struct SaveFile<'a> {
+    version: std::borrow::Cow<'a, str>,
+    values: Vec<haxe::Value<'a>>,
+}
+
+impl<'a> SaveFile<'a> {
+    fn save(save_file: &Self) -> String {
+        format!(
+            "[{version}]{hxon}",
+            version = save_file.version,
+            hxon = haxe::to_string(&save_file.values),
+        )
+    }
+
+    fn load(data: &'a mut Vec<u8>) -> Self {
+        let version_tag_end = data.iter().position(|c| *c == b']').unwrap();
+        let _ = data.drain(..=version_tag_end);
+
+        Self {
+            version: "4.0.105".into(),
+            values: haxe::from_str(std::str::from_utf8(data).unwrap()).unwrap(),
         }
     }
 }
