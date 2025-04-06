139d68444f90a72ab5403142a452fd64542f5b08
0xadk <0xadk@users.noreply.github.com>
2025-04-06 05:05:30 -0700

    feat: allow modules to be consumed as a library

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod cli;
+pub mod xxtea;
+
+pub mod crypt;
+pub mod haxe;
+pub mod savetool;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
 #![deny(clippy::pedantic)]
 
-mod cli;
-mod xxtea;
-
-mod crypt;
-mod haxe;
-mod savetool;
-
 fn main() {
-    cli::run();
+    mm2_tool::cli::run();
 }
